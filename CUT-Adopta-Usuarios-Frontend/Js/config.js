8c9c8332f9dacd95785a87170d88406a6eb0f5a0
pedrosanchezfrias09 <142287894+pedrosanchezfrias09@users.noreply.github.com>
2025-02-19 00:36:53 -0600

    Update config.js

--- CUT-Adopta-Usuarios-Frontend/Js/config.js
+++ CUT-Adopta-Usuarios-Frontend/Js/config.js
@@ -2,3 +2,11 @@
 const config = {
     API_URL: 'https://pedrocutadopta.onrender.com'
 };
+
+// ya que utilizamos el servicio de render gratutio para el backend, es necesario hacer un ping cada cierto tiempo para evitar que se duerma
+// Ping al backend cada 5 minutos
+setInterval(() => {
+    fetch('https://pedrocutadopta.onrender.com/login')
+        .then(res => console.log('Ping exitoso:', res.status))
+        .catch(err => console.error('Error en ping:', err));
+}, 300000); // Cada 5 minutos (300,000 ms)
