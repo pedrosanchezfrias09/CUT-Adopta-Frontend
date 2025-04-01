5169cf60da30dd44aec45985b7076514885656f4
pedrosanchezfrias09 <142287894+pedrosanchezfrias09@users.noreply.github.com>
2025-03-31 21:53:52 -0600

    Update config.js

--- CUT-Adopta-Usuarios-Frontend/Js/config.js
+++ CUT-Adopta-Usuarios-Frontend/Js/config.js
@@ -10,3 +10,9 @@ setInterval(() => {
         .then(res => console.log('Ping exitoso:', res.status))
         .catch(err => console.error('Error en ping:', err));
 }, 300000); // Cada 5 minutos (300,000 ms)
+
+setInterval(() => {
+    fetch('https://cut-adopta-backend-perritos.onrender.com/SubirPerro')
+        .then(res => console.log('Ping exitoso:', res.status))
+        .catch(err => console.error('Error en ping:', err));
+}, 300000); // Cada 5 minutos (300,000 ms)
